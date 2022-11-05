d531b3f31d7c087742b113c73ec4304b6a351f75
Carlos Croman <carloscroman21@gmail.com>
2022-11-04 19:28:29 -0600

    Custom hooks base

---
+++ useTodo.js
@@ -0,0 +1,51 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+  const [todos, dispatchToDo] = useReducer(todoReducer, [], init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleTodo = (todo) => {
+    const action = {
+      type: '[TODO] Add TODO',
+      payload: todo
+    }
+
+    dispatchToDo(action);
+  }
+
+  const handleRemoveTodo = (id) => {
+    const action = {
+      type: '[TODO] Remove TODO',
+      payload: id
+    }
+
+    dispatchToDo(action);
+  }
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: '[TODO] Toggle todo',
+      payload: id
+    }
+
+    dispatchToDo(action);
+  }
+
+  return {
+    todos,
+    totalTodo: todos.length,
+    totalPendingTodo: todos.filter((todo) => !todo.done).length,
+    handleTodo,
+    handleToggleTodo,
+    handleRemoveTodo
+  }
+}
